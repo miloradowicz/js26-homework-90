6de60e7891acc735b8131474e832e071c98b2070
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2025-02-03 17:49:41 +0600

    api & frontend types added

--- api/types.d.ts
+++ api/types.d.ts
@@ -3,3 +3,28 @@ import { WebSocket } from 'ws';
 export interface Connections {
   [id: string]: WebSocket;
 }
+
+export interface Point {
+  x: number;
+  y: number;
+  drag: boolean;
+  color: string;
+}
+
+export interface InboundMessage {
+  type: 'ADD_POINT';
+  payload: Point;
+}
+
+export interface OutboundMessage {
+  type: 'NEW_POINT';
+  source: string;
+  payload: Point;
+}
+
+export interface GenericError {
+  error: {
+    type: 'SYNTAX_ERROR';
+    message: string;
+  };
+}
--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -1,6 +1,28 @@
-export interface Coord {
+export interface Point {
   x: number;
   y: number;
   drag: boolean;
   color: string;
+}
+
+export interface MultiUserPointCollection {
+  [id: string]: Point[];
+}
+
+export interface InboundMessage {
+  type: 'NEW_POINT';
+  source: string;
+  payload: Point;
+}
+
+export interface OutboundMessage {
+  type: 'ADD_POINT';
+  payload: Point;
+}
+
+export interface GenericError {
+  error: {
+    type: 'SYNTAX_ERROR';
+    message: string;
+  };
 }
