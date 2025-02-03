3246c63f9933a839f9d4c9fdd96696d2b6199d56
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2025-02-03 17:50:04 +0600

    api done

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,64 @@
 import express from 'express';
+import expressWs from 'express-ws';
 import cors from 'cors';
 
+import { Connections, GenericError, InboundMessage, OutboundMessage } from './types';
 import config from './config';
-import expressWs from 'express-ws';
 
 const app = express();
 expressWs(app);
 
+const connections: Connections = {};
+
+const router = express.Router();
+
+router.ws('/paint', (ws) => {
+  const id = crypto.randomUUID();
+  connections[id] = ws;
+  console.log(`${id} connected`);
+
+  ws.on('message', (msg) => {
+    try {
+      const decoded = JSON.parse(msg.toString()) as InboundMessage;
+
+      switch (decoded.type) {
+        case 'ADD_POINT':
+          for (const connId in connections) {
+            const conn = connections[connId];
+
+            conn.send(
+              JSON.stringify({
+                type: 'NEW_POINT',
+                source: id,
+                payload: decoded.payload,
+              } as OutboundMessage)
+            );
+          }
+          break;
+      }
+    } catch (e) {
+      if (e instanceof SyntaxError) {
+        ws.send(
+          JSON.stringify({
+            error: {
+              type: 'SYNTAX_ERROR',
+              message: 'Could not parse message',
+            },
+          } as GenericError)
+        );
+        console.error(e);
+      }
+    }
+  });
+
+  ws.on('close', () => {
+    delete connections[id];
+    console.log(`${id} disconnected`);
+  });
+});
+
 app.use(cors());
+app.use(router);
 
 (async () => {
   app.listen(config.express.port, () => {
