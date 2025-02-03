6da25a988957abeb7637b9e03efccf97fe9f1b0f
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2025-02-03 09:50:27 +0600

    canvas & drawing logic added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState, useEffect, MouseEventHandler } from 'react';
+import { Card, Grid2 as Grid, hexToRgb } from '@mui/material';
+import { ColorChangeHandler, ChromePicker } from 'react-color';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Coord } from './types';
+
+const App = () => {
+  const ref = useRef<HTMLCanvasElement>(null);
+  const [context, setContext] = useState<CanvasRenderingContext2D | null>();
+
+  const [color, setColor] = useState<string>('rgb(255,0,0)');
+  const [paint, setPaint] = useState(false);
+  const [coord, setCoord] = useState<Coord[]>([]);
+
+  useEffect(() => {
+    if (ref.current) {
+      const c = ref.current;
+      setContext(c.getContext('2d'));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!context) {
+      return;
+    }
+
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+    for (let i = 0; i < coord.length; i++) {
+      context.beginPath();
+
+      if (coord[i].drag) {
+        context.moveTo(coord[i - 1].x, coord[i - 1].y);
+      } else {
+        context.moveTo(coord[i].x + 1, coord[i].y + 1);
+      }
+
+      context.lineTo(coord[i].x, coord[i].y);
+      context.closePath();
+      context.lineWidth = 2;
+      context.strokeStyle = coord[i].color;
+      context.stroke();
+    }
+  }, [context, coord]);
+
+  const handleColor: ColorChangeHandler = (color) => setColor(hexToRgb(color.hex));
+
+  const addPoint = (x: number, y: number, drag: boolean) => {
+    setCoord((coord) => [...coord, { x, y, drag, color }]);
+  };
+
+  const handleClick: MouseEventHandler<HTMLCanvasElement> = (e) => {
+    const x = e.clientX - e.currentTarget.offsetLeft;
+    const y = e.pageY - e.currentTarget.offsetTop;
+
+    setPaint(true);
+    addPoint(x, y, false);
+  };
+
+  const handleDrag: MouseEventHandler<HTMLCanvasElement> = (e) => {
+    const x = e.clientX - e.currentTarget.offsetLeft;
+    const y = e.pageY - e.currentTarget.offsetTop;
+
+    if (paint) {
+      addPoint(x, y, true);
+    }
+  };
+
+  const handleDrop = () => {
+    setPaint(false);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Grid container direction='column-reverse' sx={{ placeContent: 'center', position: 'static' }}>
+        <Grid alignSelf='end'>
+          <ChromePicker color={color} onChange={handleColor} />
+        </Grid>
+        <Grid>
+          <Card
+            raised
+            component='canvas'
+            ref={ref}
+            width='1280'
+            height='720'
+            onMouseDown={handleClick}
+            onMouseMove={handleDrag}
+            onMouseUp={handleDrop}
+            onMouseLeave={handleDrop}
+          />
+        </Grid>
+      </Grid>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/types.d.ts
@@ -0,0 +1,6 @@
+export interface Coord {
+  x: number;
+  y: number;
+  drag: boolean;
+  color: string;
+}
